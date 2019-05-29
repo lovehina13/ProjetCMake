76e4ba11410cb279db73e11fdd6e0d6c021025dd
Alexis Foerster <alexis.foerster@gmail.com>
2019-05-29 16:45:00 +0200

    Bibliothèque dynamique.

--- compile.sh
+++ compile.sh
@@ -5,4 +5,6 @@ cmake .. -DCMAKE_INSTALL_PREFIX="../install"
 cmake --build . --config Debug --target install
 cmake --build . --config Release --target install
 cd ..
+
+export LD_LIBRARY_PATH="./install/lib":${LD_LIBRARY_PATH}
 install/bin/MonExecutable
