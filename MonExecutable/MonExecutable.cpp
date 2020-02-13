6e052b32dfd3803d7771daf7f4803fdee007f300
Alexis Foerster <alexis.foerster@gmail.com>
2020-02-13 11:15:00 +0100

    Correction des fichiers CMake.

--- MonExecutable/MonExecutable.cpp
+++ MonExecutable/MonExecutable.cpp
@@ -1,11 +1,13 @@
 #include "MaClasse.h"
 #include "MaSousClasse.h"
+#include <cstdlib>
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello world" << std::endl;
     std::cout << MaClasse(1).getMonEntier() << std::endl;
-    std::cout << MaSousClasse(2, 3).getMonEntier() << " " << MaSousClasse(2, 3).getMonSousEntier() << std::endl;
-    return 0;
+    std::cout << MaSousClasse(2, 3).getMonEntier() << std::endl;
+    std::cout << MaSousClasse(2, 3).getMonSousEntier() << std::endl;
+    return EXIT_SUCCESS;
 }
